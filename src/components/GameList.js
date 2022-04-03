687f80620784397c61c459efff70fba880a4e8d0
williamsovine <william.sovine@som.re>
2022-04-03 11:01:44 -0500

    calculate er and ar

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -4,9 +4,12 @@ import GameCard from "./GameCard";
 import GameListWeekSelect from "./GameListWeekSelect";
 import {Box, Grid, Typography} from "@mui/material";
 
+
 const GameList = () => {
     const [games, setGames] = useState([]);
     const [week, setWeek] = useState({});
+    const [er, setEr] = useState(0);
+    const [ar, setAr] = useState(0);
 
     useEffect(() => {
         fetchCurrentWeek();
@@ -17,6 +20,13 @@ const GameList = () => {
             fetchGames();
         }
     }, [week]);
+
+    useEffect(() => {
+        if (Object.keys(week).length !== 0){
+            expected_dollar_return();
+            actual_dollar_return();
+        }
+    }, [games]);
 
     const api = axios.create({
         baseURL: process.env.REACT_APP_API_ENDPOINT,
@@ -44,6 +54,55 @@ const GameList = () => {
         }
     };
 
+    const profit_per_dollar = (odds) => {
+        let mult = 0;
+        if (odds < 0) {
+            mult =1 - 100 / odds
+        } else {
+            mult = 1 + odds / 100
+        }
+        return mult - 1;
+    }
+
+    const expected_dollar_return = () => {
+        setEr(0);
+        if (games !== undefined) {
+            let newEr = 0;
+            games.forEach(game => {
+                if (game.gamebetcalcs.away_ml_er > 0 && game.gamebetcalcs.away_ml_er != null){
+                    newEr += game.gamebetcalcs.away_ml_er * profit_per_dollar(game.AwayTeamMoneyLine);
+                } else if (game.gamebetcalcs.home_ml_er > 0 && game.gamebetcalcs.home_ml_er != null) {
+                    newEr += game.gamebetcalcs.home_ml_er * profit_per_dollar(game.HomeTeamMoneyLine);
+                }
+                setEr(newEr);
+            });
+        }
+    };
+
+
+    const actual_dollar_return = () => {
+        setAr(0);
+        if (games !== undefined) {
+            let newAr = 0;
+            games.forEach(game => {
+                if (game.gamebetcalcs.away_ml_er > 0 && game.gamebetcalcs.away_ml_er != null){
+                    if (game.AwayTeamScore > game.HomeTeamScore){
+                        newAr += profit_per_dollar(game.AwayTeamMoneyLine);
+                    } else {
+                        newAr -=1;
+                    }
+                } else if (game.gamebetcalcs.home_ml_er > 0 && game.gamebetcalcs.home_ml_er != null) {
+                    if (game.HomeTeamScore > game.AwayTeamScore) {
+                        newAr += profit_per_dollar(game.HomeTeamMoneyLine);
+                    } else {
+                        newAr -= 1;
+                    }
+                }
+                setAr(newAr);
+            });
+        }
+    };
+
     return (
         <Box>
             <Typography variant='h1' color='primary'>NCAA Football</Typography>
@@ -53,7 +112,19 @@ const GameList = () => {
                     seasonType={week.season_type} setWeek={setWeek}
                 /> : null
             }
-            <Box m={4} />
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                    <Typography m={2} color='secondary'>
+                        Expected Return per $ : {er.toFixed(2)}
+                    </Typography>
+                    <Grid item xs={12} sm={6}>
+                        <Typography m={2} color='secondary'>
+                            Actual Return per $ : {ar.toFixed(2)}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+
             <Grid container spacing={0.5}>
                 {games.map(game => (
                     <Grid item key={game.GameID} xs={6} sm={4} md={3}>
